83659d8d0b681fec6b3ca492087f33515260873c
GABRIEL SILVA DE AZEVEDO <gabriel7azevedo@gmail.com>
2024-05-30 18:15:50 -0300

    Altera rota

--- src/server.js
+++ src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas a API do AllBooks")
-  console.log("API disponível através da URL http://localhost:8000")
-=======
   console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 620cbbbb4b03137211ee824f05ac5b3e24acac57
 })
